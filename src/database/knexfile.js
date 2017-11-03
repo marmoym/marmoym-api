3c1ff661c68021113cee676d99324b84d7c14e2a
Engine S. Park <enginehenryed@gmail.com>
2017-11-03 21:05:07 +0900

    Add script `start:dev` to start knex with `debug` enabled
    PR-URL: https://github.com/tymsai/marmoym-api/pull/146

--- src/database/knexfile.js
+++ src/database/knexfile.js
@@ -1,9 +1,12 @@
-const dbConfig = require('../../src/config/marmoym-config').dbConfig;
 const path = require('path');
+
+const dbConfig = require('../../src/config/marmoym-config').dbConfig;
 
 const _config = dbConfig.marmoym_dev1;
 const MIGRATION_PATH = path.resolve(__dirname, 'migrations');
 const SEED_PATH = path.resolve(__dirname, 'seeds');
+
+_config.debug = process.argv[2] === '--debug' ? true : false;
 
 _config.development.migrations = {
   directory: MIGRATION_PATH
