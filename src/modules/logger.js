c1c66ecfefe8a5070194d3ebc7af7dc766bec169
Engine S. Park <enginehenryed@gmail.com>
2017-11-05 19:02:31 +0900

    Modify logger to log to file only when `production`
    PR-URL: https://github.com/tymsai/marmoym-api/pull/154

--- src/modules/logger.js
+++ src/modules/logger.js
@@ -7,30 +7,38 @@ const LOG_PATH = path.resolve(__dirname, '..', '..', 'log');
 const tsFormat = () => (new Date()).toLocaleTimeString();
 const LOG_LEVEL = process.env.NODE_ENV === 'development' ? 'debug' : 'info';
 
+// Create the directory if it does not exist
 if (!fs.existsSync(LOG_PATH)) {
-  winston.warn(`Create log directory ${LOG_PATH}`);
-  // Create the directory if it does not exist
+  winston.warn(`Creating log path at ${LOG_PATH}`);
   fs.mkdirSync(LOG_PATH);
 }
 
+const _transports = [
+  new (winston.transports.Console)({
+    timestamp: tsFormat,
+    colorize: true,
+    level: LOG_LEVEL,
+  }),
+];
+
+const dailyRotateLogger = new (winston.transports.DailyRotateFile)({
+  filename: `${LOG_PATH}/log`,
+  timestamp: tsFormat,
+  datePattern: 'yyyy-MM-dd.',
+  prepend: true,
+  level: LOG_LEVEL,
+});
+
+if (process.env.NODE_ENV === 'production') {
+  _transports.push(dailyRotateLogger);
+}
+
 const logger = new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)({
-      timestamp: tsFormat,
-      colorize: true,
-      level: LOG_LEVEL,
-    }),
-    new (winston.transports.DailyRotateFile)({
-      filename: `${LOG_PATH}/log`,
-      timestamp: tsFormat,
-      datePattern: 'yyyy-MM-dd.',
-      prepend: true,
-      level: LOG_LEVEL,
-    }),
-  ],
+  transports: _transports,
 });
 
 logger.warn('[BEGIN] ================== winston.js logger check ==================');
+logger.warn(`winston is set with ${logger._names.length} loggers`)
 logger.debug('winston debug');
 logger.verbose('winston verbose');
 logger.info('winston info');
