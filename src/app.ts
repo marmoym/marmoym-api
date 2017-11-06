4ee5a5a911ac2a11b57d3e8021f146e2072da2d4
Engine S. Park <enginehenryed@gmail.com>
2017-11-07 00:44:33 +0900

    Remove winston global settings out of `app.ts`
    PR-URL: https://github.com/tymsai/marmoym-api/pull/163

--- src/app.ts
+++ src/app.ts
@@ -4,22 +4,15 @@
 import * as express from 'express';
 import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
-import * as winston from 'winston';
+
 import { serverConfig, logConfig } from './config/marmoym-config'
 import * as errorHandleService from './services/errorHandleService';
 import routes from './routes';
 import db from '@src/database';
+import * as logger from '@src/modules/logger';
 
 const app: express.Application = express();
 const port: number = process.env.PORT || serverConfig['marmoym-dev1'].port;
-
-winston.configure({
-  transports: [
-    new winston.transports.Console({
-      level: logConfig['marmoym-dev1'].logLevel,
-    }),
-  ],
-});
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,7 +21,7 @@ app.use("/", routes);
 app.use(errorHandleService.handleError);
 
 app.listen(serverConfig['marmoym-dev1'].port, () => {
-  winston.debug(`Listening on ${serverConfig['marmoym-dev1'].port}`);
+  logger.debug(`[BEGIN] Express listening on ${serverConfig['marmoym-dev1'].port}`);
 });
 
 export default app;
