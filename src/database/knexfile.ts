9430c3b3b5fecbea765e27b2e22c6f5f94f974bf
eldeni <eldeniyenden@gmail.com>
2018-04-08 19:27:02 +0900

    hotfix: modify dbConfig reference

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -2,16 +2,19 @@ import * as path from 'path';
 
 import dbConfig from '@config/dbConfig';
 
-const _config = dbConfig.marmoym_dev1;
 const MIGRATION_PATH = path.resolve(__dirname, 'migrations');
 const SEED_PATH = path.resolve(__dirname, 'seeds');
 
-_config.development.migrations = {
+const knexfile = {
+  ...dbConfig,
+};
+
+knexfile.development.migrations = {
   directory: MIGRATION_PATH
 };
 
-_config.development.seeds = {
+knexfile.development.seeds = {
   directory: SEED_PATH
 };
 
-export default _config;
+export default knexfile;
