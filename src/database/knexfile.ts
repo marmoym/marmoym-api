563fe0d3a1cfa83bfe2bd0f7f7d03239a86a6a02
eldeni <eldeniyenden@gmail.com>
2018-04-08 19:35:58 +0900

    hotfix: modify post-script in deployment

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -5,10 +5,12 @@ import dbConfig from '@config/dbConfig';
 const MIGRATION_PATH = path.resolve(__dirname, 'migrations');
 const SEED_PATH = path.resolve(__dirname, 'seeds');
 
+console.log(123, dbConfig);
 const knexfile = {
   ...dbConfig,
 };
 
+console.log(1234, knexfile);
 knexfile.development.migrations = {
   directory: MIGRATION_PATH
 };
